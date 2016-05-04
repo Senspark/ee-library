2cb35b9761b5250bd2dfd9bf273248d1b2d39986
enrevol <enrevol@gmail.com>
2016-05-04 09:41:25 +0700

    - Fix Android mk.

---
+++ src/proj.android/jni/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+# Warning flags.
+# LOCAL_CFLAGS += -Wdouble-promotion -Wignored-qualifiers -Wundef
+LOCAL_CFLAGS += -Wchar-subscripts -Wcomment -Wformat=2 -Wnonnull -Winit-self
+LOCAL_CFLAGS += -Wmain -Wmissing-braces -Wmissing-include-dirs -Wparentheses -Wsequence-point
+LOCAL_CFLAGS += -Wreturn-type -Waddress -Wmissing-declarations -Wmissing-field-initializers
+LOCAL_CFLAGS += -Wunused-function -Wunused-label -Wuninitialized
+
+LOCAL_MODULE := ee_static
+
+LOCAL_MODULE_FILENAME := libee
+
+LOCAL_SRC_FILES := ${shell find $(LOCAL_PATH)/../../Classes -name "*.cpp" ! -name "__*.cpp" - print}
+LOCAL_SRC_FILES += ${shell find $(LOCAL_PATH)/../../../boost_1_60_0/boost -name "*.cpp" -print}
+                   
+LOCAL_C_INCLUDES := \
+$(LOCAL_PATH)/../../Classes \
+$(LOCAL_PATH)/../../cocos2d \
+$(LOCAL_PATH)/../../cocos2d/cocos \
+$(LOCAL_PATH)/../../cocos2d/cocos/editor-support \
+$(LOCAL_PATH)/../../cocos2d/cocos/platform/android/jni \
+$(LOCAL_PATH)/../../cocos2d/cocos/ui \
+$(LOCAL_PATH)/../../cocos2d/extensions
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../../boost_1_60_0
+
+include $(BUILD_STATIC_LIBRARY)

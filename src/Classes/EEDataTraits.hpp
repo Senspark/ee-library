e7f95e683caafc8995dfc6976762270712be71da
Nikel Arteta <nikelarteta@Thanhs-Mac-mini.local>
2016-12-16 17:00:01 +0700

    Add chrono support for DataTraits.

--- src/Classes/EEDataTraits.hpp
+++ src/Classes/EEDataTraits.hpp
@@ -9,6 +9,7 @@
 #ifndef EE_DATA_TRAITS_HPP_
 #define EE_DATA_TRAITS_HPP_
 
+#include <chrono>
 #include <string>
 #include <type_traits>
 
@@ -39,6 +40,32 @@ public:
 
     static T get(const std::string& value) {
         return static_cast<T>(DataTraits<U>::get(value));
+    }
+};
+
+namespace detail {
+template <class T> struct is_duration : std::false_type {};
+
+template <class Rep, class Period>
+struct is_duration<std::chrono::duration<Rep, Period>> : std::true_type {};
+} // namespace detail
+
+template <class T>
+struct DataTraits<T, std::enable_if_t<detail::is_duration<T>::value>> {
+private:
+    using Rep = typename T::rep;
+
+public:
+    template <class U, std::enable_if_t<detail::is_duration<U>::value, int> = 0>
+    static std::string set(const U& value) {
+        return DataTraits<Rep>::set(
+            std::chrono::duration_cast<T>(value).count());
+    }
+
+    static std::string set(Rep value) { return DataTraits<Rep>::set(value); }
+
+    static T get(const std::string& value) {
+        return T(DataTraits<Rep>::get(value));
     }
 };
 } // namespace ee

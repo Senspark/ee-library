b9dc5673eef5b829e264931d8b61064f8fa2cfa1
eps <enrevol@gmail.com>
2018-04-09 11:21:08 +0700

    Forward more classes.

--- src/Classes/EECocos2dxFwd.hpp
+++ src/Classes/EECocos2dxFwd.hpp
@@ -136,6 +136,8 @@ class Mat4;
 class Size;
 class Rect;    
 struct Color3B;
+struct Color4F;
+struct BlendFunc;
 
 class GLProgram;
 class GLProgramState;
@@ -173,6 +175,9 @@ class HttpResponse;
 } // namespace cocos2d
 
 namespace cocosbuilder {
+struct BlockData;
+struct BlockControlData;
+    
 class CCBReader;
 class NodeLoaderLibrary;
 class NodeLoader;
